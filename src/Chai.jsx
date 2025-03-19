48766958995fd53f16149354ca6eb1c713e820a8
unknown <christeenpaul2@gmail..com>
2025-03-19 20:18:27 +0530

    second commit

---
+++ src/Chai.jsx
@@ -0,0 +1,12 @@
+function Chai(){
+    return(
+    <>
+     <h1>Let's Lern React</h1>
+     <p>first we intsalled node and then as we know we install some package from npm install but in this we can directliy exicute usibg npx (node package exicuter).after that we created a repo in github so we can directly post in the github while coding their are many ways to execute react we used vite+react because it is fater go through the  react documentation  after installing go through the package.jason file and analyse you will se screipt tag which is important to know after that go to src and you will see a folder named app.jsx etc main
+        .jsx the its same like jsx its just that they have creted a name jsx in vite for some disgution compared to the normal veraion remove all unnecssery files after that main and app will be left you will se we using App in the js file thi
+        s is the advantage of js we can code html in js by using app App as you can see in the file then we created a new file in sec called chai.jsx then created a fuction Chai returned an html note we are using campital C for chai and all which is the best practice in App file  we  imported chai file  retur chai file and exported the file to main.jsx which is the main file in main we impoted add and  printed app in the root node  </p>
+    </> 
+    )
+}
+
+export default Chai
